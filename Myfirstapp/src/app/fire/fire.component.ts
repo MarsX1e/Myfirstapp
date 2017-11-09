85292d9a3082b9115b1fb454545c6e4018f66827
MarsX1e <tianyuxie14@gmail.com>
2017-11-09 08:06:37 -0800

    done

---
+++ Myfirstapp/src/app/fire/fire.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-fire',
+  templateUrl: './fire.component.html',
+  styleUrls: ['./fire.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class FireComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
